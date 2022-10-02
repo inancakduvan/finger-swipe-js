303f1fe3d6b39d68ca03dd707d8a5af0979166c0
İnanç Akduvan <inancakduvan@Inancs-MacBook-Air.local>
2022-10-02 13:55:03 +0300

    vertical demo added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <FingerJs
-        direction='horizontal'
+        direction='vertical'
         onRight={() => {
           alert('hey right');
         }}
